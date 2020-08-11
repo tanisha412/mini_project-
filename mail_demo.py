220c5db1e27fb362a14e977a7050654e0a6514e2
tanisha412 <62757370+tanisha412@users.noreply.github.com>
2020-08-12 00:26:14 +0530

    Add files via upload

--- mail_demo.py
+++ mail_demo.py
@@ -1,17 +1,25 @@
-import auth
+
+import os
 import smtplib
 from email.message import EmailMessage
 
-EMAIL_ADDRESS = auth.EMAIL_USER
-EMAIL_PASSWORD = auth.EMAIL_PASS
-
-
+EMAIL_ADDRESS = 'tanighunawat412@gmail.com'
+EMAIL_PASSWORD = 'rlwxrhuvgciwebdh'
 
 msg = EmailMessage()
-msg['Subject'] = 'HY LOVE'
+msg['Subject'] = 'HY '
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = 'atilondiya@gmail.com'
-msg.set_content('HELLO ,Whats up?')
+msg.set_content('this is plain text msg')
+
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1 style = "color:blue;"> This is an HTML email </h1>
+   <body>
+<html>
+""" , subtype = 'html')
 
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
     smtp.login(EMAIL_ADDRESS , EMAIL_PASSWORD)
