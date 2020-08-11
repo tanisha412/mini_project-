442dfc7f3ff7fc80f5bd9aef0867bcbfdc3623c9
Anshultilondiya <43814313+Anshultilondiya@users.noreply.github.com>
2020-08-12 01:15:51 +0530

    Pull this

--- mail_demo.py
+++ mail_demo.py
@@ -1,4 +1,3 @@
-
 import os
 import smtplib
 from email.message import EmailMessage
@@ -6,17 +5,28 @@ from email.message import EmailMessage
 EMAIL_ADDRESS = 'tanighunawat412@gmail.com'
 EMAIL_PASSWORD = 'rlwxrhuvgciwebdh'
 
+def rowExtractor(row):
+    import gspread
+    gc = gspread.service_account(filename='google_auth.json')
+    sheet = gc.open('Scribble Collector')
+    worksheet = sheet.sheet1
+    Row = worksheet.row_values(row)
+    worksheet.update_cell(row,7, 'Bingo!')
+    return [Row[2],Row[3],Row[4],Row[5]]
+
+Sender , Receiver,ReceiverMail, Message  = rowExtractor(10)
+
 msg = EmailMessage()
-msg['Subject'] = 'HY '
+msg['Subject'] = 'Hey !' + Sender + 'you have a new scribble'
 msg['From'] = EMAIL_ADDRESS
-msg['To'] = 'atilondiya@gmail.com'
+msg['To'] = ReceiverMail
 msg.set_content('this is plain text msg')
 
 msg.add_alternative("""\
 <!DOCTYPE html>
 <html>
   <body>
-    <h1 style = "color:blue;"> This is an HTML email </h1>
+    <h1 style = "color:blue;">""" + Message + """</h1>
    <body>
 <html>
 """ , subtype = 'html')
--- sheets.py
+++ sheets.py
@@ -1,9 +1,18 @@
-import gspread
-gc = gspread.service_account(filename='google_auth.json')
-sheet = gc.open('Scribble Collector')
 
-worksheet = sheet.sheet1
+def rowExtractor(row):
+    import gspread
+    gc = gspread.service_account(filename='google_auth.json')
+    sheet = gc.open('Scribble Collector')
+    worksheet = sheet.sheet1
+    Row = worksheet.row_values(row)
+    # worksheet.update(row, 'Bingo!')
+    return [Row[2],Row[3],Row[4]]
 
-values_list = worksheet.row_values(2)
+Sender , Receiver, Message  = rowExtractor(3)
 
-print(values_list)
+
+
+
+
+
+
